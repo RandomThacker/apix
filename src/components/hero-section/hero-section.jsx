8c2e883e527df38cb47273fbb63c3d0597a9cef5
Aryan Thacker <randomthacker@gmail.com>
2025-02-23 10:20:20 +0530

    feat: updated hero section

--- src/components/hero-section/hero-section.jsx
+++ src/components/hero-section/hero-section.jsx
@@ -16,7 +16,7 @@ export default function HeroSection() {
     return (
         <>
             <div
-                className="h-[50rem] w-full bg-black relative flex flex-col items-center justify-center"
+                className="h-[100vh] w-full bg-black relative flex flex-col items-center justify-center"
                 style={{
                     backgroundImage: `linear-gradient(rgba(255,255,255,0.2) 1px, transparent 1px), 
                       linear-gradient(90deg, rgba(255,255,255,0.2) 1px, transparent 1px)`,
@@ -28,7 +28,7 @@ export default function HeroSection() {
 
                 {/* Apply the font ONLY to the Hero Text */}
                 <p
-                    className={`text-4xl mb-4 sm:text-8xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 ${pixelifySans.className}`}
+                    className={`text-4xl mb-4 sm:text-9xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 ${pixelifySans.className}`}
                 >
                     Api-X
                 </p>
@@ -36,19 +36,18 @@ export default function HeroSection() {
                 <AnimatedShinyTextDemo />
             </div>
 
-            <section className="container mx-auto px-4 py-20 text-center bg-[#0d0c0c]">
+            <section className="container mx-auto px-4 py-32 text-center bg-[#0d0c0c]">
                 {/* Apply the font ONLY to the heading */}
-                <h1 className={`text-5xl font-bold text-white mb-10`}>
+                <h1 className={`text-5xl font-bold text-white mb-20`}>
                     API Testing, but with Superpowers!🚀
                 </h1>
                 <LinkPreviewSection />
                 <div className="flex justify-center gap-4 mt-12">
-                    <Button className="bg-neutral-800 text-white hover:bg-neutral-700">Documentation</Button>
-                    <Button variant="secondary">
+                    <Button className="bg-neutral-800 text-white hover:bg-neutral-700">View Documentation</Button>
+                    <Button className="bg-gradient-to-r from-[#db2c21] to-[#ff6a00] text-black font-semibold hover:brightness-110 transition-all duration-300">
                         View on GitHub <ChevronRight className="ml-2 h-4 w-4" />
                     </Button>
                 </div>
-
             </section>
         </>
     );
